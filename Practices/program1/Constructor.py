94e4624321fe870901a5ad9c5e1016fb32fbd44a
NilanjanaBala <nilanjanabala14@gmail.com>
2020-03-13 16:06:20 +0000

    new program

--- Practices/program1/Constructor.py
+++ Practices/program1/Constructor.py
@@ -3,3 +3,9 @@ git add README.md
 git commit -m "first commit"
 git remote add origin https://github.com/NilanjanaBala/Python.git
 git push -u origin master
+
+ git config --global user.email "nilanjanabala14@gmail.com"
+ git config --global user.name "NilanjanaBala"
+ 
+ 
+ # comment
