e1dfed36398d90bd6291fe65d9351aec291e84f9
NilanjanaBala <nilanjanabala14@gmail.com>
2020-03-18 23:15:59 +0000

    Login to mytestinglabs selenium automation

--- PycharmProjects/Selenium/Selenium_1.py
+++ PycharmProjects/Selenium/Selenium_1.py
@@ -12,6 +12,8 @@ driver.find_element_by_name("password").send_keys("abc123")
 driver.find_element_by_name("cnfpassword").send_keys("abc123")
 driver.find_element_by_name("first_name").send_keys("Nilanjana")
 driver.find_element_by_name("last_name").send_keys("Bala")
+driver.find_element_by_class_name("username").send_keys("Nilanjana")
+driver.find_element_by_id("textarea").send_keys("abc")
 driver.find_element_by_id("agree").click()
 driver.find_element_by_id("submitForm").click()
 
